26c14438fc3e76224ce1ef261b0d6632dd9fe199
Timur Safin <tsafin@tarantool.org>
2022-07-13 02:20:14 +0300

    lpeg: processing queue
    Keep all references rst files in the prcessing queue. Enqueue new
    items to the tail, dequeue from the head. Keep all known elements
    in the list.
    
    * net_box problems
    
      We used to use wrong pattern regex for matchin whitespaces,
      which coincidentally consumed 'n' instead of new-line (which
      should not be allowed as whitespace delimiter in that place, BTW)
    
      Fixed via replacing of referencing to '%s' instead.
    
    * correct processing of boundary conditions
    
      Last item in toctree references list was not processed if there
      was a missing new line. Corrected.
    
    * reformatted using 4 spaces indent

--- parse-rst.lua
+++ parse-rst.lua
@@ -5,31 +5,86 @@ local re = require 'relabel'
 local fio = require 'fio'
 
 local index_rst = '../tarantool-doc/doc/reference/reference_lua/index.rst'
+local basepath
+local fulltext
 
-local file = fio.open(index_rst, 'O_RDONLY')
-local fulltext = file:read()
+local backlog = {}
+local verbose = true
 
-local toctree = {}
+local function report_file(path)
+    if verbose and path ~= nil then
+        print('processing: ', path)
+    end
+end
 
-local pattern = re.compile ([[ --lpeg
-        RST             <- (TocTree / SkipLine )*
-        SkipLine        <- {[^%nl]* %nl}
-        TocTree         <- TocTreeHeader SkipHeaders References
-        SkipHeaders     <- (WsIndent ':' keyword ':' [^%nl]* %nl)* %nl
-        TocTreeHeader   <- '..' [ ] { 'toctree' }'::' [^%nl]* %nl
-        References      <- (WsIndent Reference %nl)*
-        Reference       <- { [a-zA-Z0-9_]+ } -> matchr
-        WsIndent        <- [ \t\n]^+4
-        keyword         <- {[a-zA-Z]+}
+local function file_open(path)
+    if path == nil then
+        fulltext = nil
+        return
+    end
+    basepath = fio.dirname(path)
+    local currfile = fio.open(path, 'O_RDONLY')
+    fulltext = currfile ~= nil and currfile:read()
+    report_file(path)
+end
+
+local function enqueue_file(ref)
+    local fullpath = fio.pathjoin(basepath, ref .. '.rst')
+    if not backlog[fullpath] then
+        table.insert(backlog, fullpath)
+        backlog[fullpath] = true
+    end
+end
+
+local function dequeue_file()
+    if #backlog < 1 then
+        return nil
+    end
+    -- backlog[head] = nil -- do not clean, to not process twice
+    return table.remove(backlog, 1)
+end
+
+local function report_queue()
+    if not verbose then
+        return
+    end
+    if #backlog < 1 then
+        print('backlog is empty')
+        return
+    end
+    for key, v in ipairs(backlog) do
+        print(('%d. %s'):format(key, v))
+    end
+end
+
+local pattern = re.compile([[ --lpeg
+    RST             <- (TocTree / SkipLine )*
+    SkipLine        <- {[^%nl]* %nl}
+    TocTree         <- TocTreeHeader SkipHeaders References
+    SkipHeaders     <- (WsIndent ':' keyword ':' [^%nl]* %nl)* %nl
+    TocTreeHeader   <- '..' [ ]^+1 { 'toctree' }'::' [^%nl]* %nl
+    References      <- (WsIndent Reference %nl? / %nl)*
+    Reference       <- { [a-zA-Z0-9_/]+ } -> matchr
+    WsIndent        <- %s^+4
+    keyword         <- {[a-zA-Z]+}
 ]], {
-        matchr = function (s)
-                table.insert(toctree, s)
-                -- print('ref', require'json'.encode(s), ...)
-        end,
+    matchr = function(s) enqueue_file(s) end,
+    -- match_skip = function(s) print('...', s) end,
 })
 
-local T = pattern:match(fulltext)
--- print(require'json'.encode(T))
-print(require'json'.encode(toctree))
+-- start from root documentation index
+file_open(index_rst)
+
+while fulltext do
+    pattern:match(fulltext)
+    -- then continue with all submitted references
+    local next = dequeue_file()
+    if next == nil then
+        break
+    end
+    file_open(next)
+    -- stop if we have failed to open any of enqueued files
+end
+report_queue()
 
 os.exit(0)
